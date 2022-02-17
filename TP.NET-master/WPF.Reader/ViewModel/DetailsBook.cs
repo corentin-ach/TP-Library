9acf660cbae5730b79985397db98706676ece900
Nisserine-19 <n.ezzaimi@net.estia.fr>
2022-02-17 15:05:34 +0100

    Detail et Read complet

--- TP.NET-master/WPF.Reader/ViewModel/DetailsBook.cs
+++ TP.NET-master/WPF.Reader/ViewModel/DetailsBook.cs
@@ -9,7 +9,7 @@ namespace WPF.Reader.ViewModel
     public class DetailsBook : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
-        public ICommand ReadCommand { get; init; } = new RelayCommand(x => { Ioc.Default.GetRequiredService<INavigationService>().Navigate<ReadBook>(x); });
+        public ICommand ReadCommand { get; init; } = new RelayCommand(book => { Ioc.Default.GetRequiredService<INavigationService>().Navigate<ReadBook>(book); });
         // public ObservableCollection<Book> Books => Ioc.Default.GetRequiredService<LibraryService>().Books;
         /* ItemSelectedCommand = new RelayCommand(book => { Ioc.Default.GetRequiredService<INavigationService>().Navigate<DetailsBook>(book); }); */
 
--- TP.NET-master/WPF.Reader/ViewModel/ReadBook.cs
+++ TP.NET-master/WPF.Reader/ViewModel/ReadBook.cs
@@ -1,5 +1,8 @@
-﻿using System.ComponentModel;
+﻿using Microsoft.Toolkit.Mvvm.DependencyInjection;
+using System.ComponentModel;
+using System.Windows.Input;
 using WPF.Reader.Model;
+using WPF.Reader.Service;
 
 namespace WPF.Reader.ViewModel
 {
@@ -7,13 +10,23 @@ namespace WPF.Reader.ViewModel
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
+        //public ICommand GoBack { get; init; } = new RelayCommand(x => { Ioc.Default.GetRequiredService<INavigationService>().Navigate<DetailsBook>(x); });
+
         // A vous de jouer maintenant
+        public Book CurrentBook { get; init; }
+
+        public ReadBook(Book book)
+        {
+            CurrentBook = book;
+        }
+
     }
+    
 
     /* Cette classe sert juste a afficher des donnée de test dans le designer */
     class InDesignReadBook : ReadBook
     {
-        public InDesignReadBook() : base()
+        public InDesignReadBook() : base(new Book() /*{ Title = "Test Book" }*/) 
         {
         }
     }
