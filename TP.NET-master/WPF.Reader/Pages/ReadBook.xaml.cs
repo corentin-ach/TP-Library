c94b35ad91da2d606b1475a2f56314f1996cd4c4
sheeeeesh <61969570+sheeeeesh@users.noreply.github.com>
2022-02-18 10:26:18 +0100

    Design ReadBook Page

--- TP.NET-master/WPF.Reader/Pages/ReadBook.xaml.cs
+++ TP.NET-master/WPF.Reader/Pages/ReadBook.xaml.cs
@@ -24,5 +24,10 @@ namespace WPF.Reader.Pages
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
